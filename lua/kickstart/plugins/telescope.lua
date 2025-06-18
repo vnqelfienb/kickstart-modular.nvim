e89e1f1d8adc464c73fd5583041effdd077bba35
vnqelfienb <vnqelfienb@gmail.com>
2025-06-18 22:07:16 +0300

    telescope ignorsls

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -62,6 +62,9 @@ return {
         --   },
         -- },
         -- pickers = {}
+        defaults = {
+          file_ignore_patterns = { 'node_modules', 'obj', 'bin' },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
